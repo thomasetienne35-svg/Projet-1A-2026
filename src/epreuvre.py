f34426cea79d7f737461eff18d028692c4e63634
Ilouse <ilan042005@gmail.com>
2026-03-11 15:26:59 +0000

    daigramme uml modifié et ajout de la classe épreuve

---
+++ src/epreuvre.py
@@ -0,0 +1,9 @@
+from competition import Competiton
+from resultat import Resultat
+from participant import Participant
+
+class Epreuve(
+    def __init__(id_epreuve : int , categorie : str , statut : bool) -> None:
+        id_epreuve 
+
+)
