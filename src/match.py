e8f98b20f2d9b57e26b631912e909e94fdbf15d7
Ilouse <ilan042005@gmail.com>
2026-04-02 14:24:47 +0000

    màj

---
+++ src/match.py
@@ -0,0 +1,7 @@
+class Match:
+    def __init__(self, match_id: int, home_team__id: int, away_team_id: int) -> None:
+        self.match_id = match_id,
+        self.home_team__id = home_team__id,
+        self.away_team_id = away_team_id
+ 
+ 
