d599894e406074c26f8bc154ab2b1911a42a1737
Ilouse <ilan042005@gmail.com>
2026-04-02 14:58:13 +0000

    début classe match

--- src/match.py
+++ src/match.py
@@ -1,5 +1,8 @@
+from sport.py import Sport
+from joueur.py import Joueur
+
 class Match:
-    def __init__(self, match_id: int, home_team__id: int, away_team_id: int) -> None:
+    def __init__(self, id : int, sport : Sport, liste_joueur : list[Joueur]) -> None:
         self.match_id = match_id,
         self.home_team__id = home_team__id,
         self.away_team_id = away_team_id
