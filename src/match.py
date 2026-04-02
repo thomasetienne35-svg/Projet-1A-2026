80b144002853dc17a08a32e5262e1fc57e394cba
Ilouse <ilan042005@gmail.com>
2026-04-02 15:02:28 +0000

    classe match rajout

--- src/match.py
+++ src/match.py
@@ -1,10 +1,9 @@
-from sport.py import Sport
-from joueur.py import Joueur
+from sport import Sport
+from player import Player
 
 class Match:
-    def __init__(self, id : int, sport : Sport, liste_joueur : list[Joueur]) -> None:
-        self.match_id = match_id,
-        self.home_team__id = home_team__id,
-        self.away_team_id = away_team_id
- 
- 
+    def __init__(self, id : int, sport : Sport, list_home_player : list[Player], list_away_player : list[Player]) -> None:
+        self.id = id
+        self.sport = sport
+        self.list_home_player = list_home_player
+        self.list_away_player = list_away_player
