d560a8c586898cb0fc310bc3ca23a18106e68395
Thomas Etienne <thomasetienne35@gmail.com>
2026-04-07 14:32:28 +0200

    màj

--- src/loaders_player/basketball_player_loader.py
+++ src/loaders_player/basketball_player_loader.py
@@ -8,14 +8,14 @@ pd.options.display.max_columns = 100
 # Ce qu'on veut mettre dans notre Player : Nom, nationalité, date_naissance, genre
 
 
-class FootballPlayerLoader:
+class BasketballPlayerLoader:
     def __init__():
         pass
 
     def load_all_player():
         res = []
         df_football = pd.read_csv(
-            "/home/onyxia/work/Projet-1A-2026/data/football_european_leagues_tdd/player.csv"
+            "/home/onyxia/work/Projet-1A-2026/data/basketball/player.csv"
         )
         for i in range(len(df_football)):
             joueur = Player(None, None, None, "H")
