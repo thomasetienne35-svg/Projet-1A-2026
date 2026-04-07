9750a8d28c6c41746dc9d3dd255835245cdaa6f0
El-Genius-roh-roh <julesfrulli@gmail.com>
2026-04-07 14:12:28 +0200

    Rajout de la classe sport (vide pour l'instant)

--- src/football_player_loader.py
+++ src/football_player_loader.py
@@ -1,25 +1,25 @@
 from player import Player
 from sport import Sport
 
-import csv
-import os
-import pickle
-
-import numpy as np
 import pandas as pd
+
 pd.options.display.max_columns = 100
 
-#Ce qu'on veut mettre dans notre Player : Nom, nationalité, date_naissance, genre
+# Ce qu'on veut mettre dans notre Player : Nom, nationalité, date_naissance, genre
 
-class FootballPlayerLoader():
+
+class FootballPlayerLoader:
     def __init__():
         pass
+
     def load_all_player():
         res = []
-        df_football = pd.read_csv("/home/onyxia/work/Projet-1A-2026/data/football_european_leagues_tdd/player.csv")
-        for i in range (len(df_football)):
+        df_football = pd.read_csv(
+            "/home/onyxia/work/Projet-1A-2026/data/football_european_leagues_tdd/player.csv"
+        )
+        for i in range(len(df_football)):
             joueur = Player(None, None, None, "H")
-            joueur.nom = df_football.loc[i,"player_name"]
-            joueur.data_naissance = df_football.loc[i,"player_name"]
+            joueur.nom = df_football.loc[i, "player_name"]
+            joueur.data_naissance = df_football.loc[i, "player_name"]
             res.append(joueur)
         return res
