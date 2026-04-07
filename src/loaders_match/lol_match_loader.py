9b9e8a9ca1bc8bc17c9aa44edb4bdad8544eb874
Ilouse <ilan042005@gmail.com>
2026-04-07 14:36:15 +0000

    lol match loader

--- src/loaders_match/lol_match_loader.py
+++ src/loaders_match/lol_match_loader.py
@@ -12,15 +12,33 @@ class LolMatchLoader:
 
     def load_all_match(self):
         res = []
-        df_lol = pd.read_csv(
+        df_lol_match = pd.read_csv(
             "/home/onyxia/work/Projet-1A-2026/data/league_of_legends_tdd/match.csv"
         )
-        for i in range(len(df_lol)):
+        df_lol_team = pd.read.csv(
+            "/home/onyxia/work/Projet-1A-2026/data/league_of_legends_tdd/team.csv"
+        )
+        df_lol_player = pd.read.csv(
+            "/home/onyxia/work/Projet-1A-2026/data/league_of_legends_tdd/player.csv"
+        )
+        for i in range(len(df_lol_match)):
             match = Match(None, Lol, None, None)
-            match.id = df_lol.loc[i, "id"]
-            colonnes_home = ["home_team_goal"] + [f"home_player_{i}" for i in range (1,12)]
-            match.list_home_player = df.lol.loc[i, colonnes_home].tolist()
-            colonnes_away = ["away_team_goal"] + [f"away_player_{i}" for i in range (1,12)]
-            match.list_away_player = df_lol.loc[i,colonnes_away].tolist()
+            match.id = i + 2
+            abbr_blue = df_lol_match.loc[i, "team_blue"]
+            abbr_red = df_lol_match.loc[i, "team_red"]
+            equipe_filtree_blue = df_lol_team[df_lol_team["team_abbreviation"] == abbr_blue]
+            equipe_filtree_red = df_lol_team[df_lol_team["team_abbreviation"] == abbr_red]
+            if not equipe_filtree_blue.empty:
+                nom_complet_team = equipe_filtree_blue.iloc[0]["team"]
+                liste_noms_joueurs_home = df_lol_player[df_lol_player["team"] == nom_complet_team]["name"].tolist()
+                match.list_home_player = liste_noms_joueurs_home
+            else:
+                match.list_home_player = []
+            if not equipe_filtree_red.empty:
+                nom_complet_team = equipe_filtree_red.iloc[0]["team"]
+                liste_noms_joueurs_away = df_lol_player[df_lol_player["team"] == nom_complet_team]["name"].tolist()
+                match.list_away_player = liste_noms_joueurs_away
+            else:
+                match.list_away_player = []
             res.append(match)
         return res
