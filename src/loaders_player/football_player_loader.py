2cfb71b5b10a68ec8dbbbca4c77b10a33cda93c0
El-Genius-roh-roh <julesfrulli@gmail.com>
2026-04-07 16:28:02 +0200

    correction ffootball player loader + complément de sport

--- src/loaders_player/football_player_loader.py
+++ src/loaders_player/football_player_loader.py
@@ -16,8 +16,10 @@ class FootballPlayerLoader:
             "c:/Users/id2959/Desktop/Projet-1A-2026/data/football_european_leagues_tdd/player.csv"
         )
         for i in range(len(df_football)):
-            joueur = Player(None, None, None, "H", taille, poids, None)
+            joueur = Player(None, None, None, "H", None, None, None)
             joueur.prenom_nom = df_football.loc[i, "player_name"]
             joueur.data_naissance = df_football.loc[i, "birthday"]
+            joueur.poids = df_football.loc[i, "weight"]
+            joueur.taille = df_football.loc[i, "height"]
             res.append(joueur)
         return res
--- src/sport.py
+++ src/sport.py
@@ -1,3 +1,5 @@
 class Sport:
-    def __init__(self):
-        pass
+    def __init__(self, name) -> None:
+        if name not in ["football", "tennis", "volley", "basketball", "lol"]:
+            raise ValueError("Ce sport n'est pas pris en charge")
+        self.name = name
