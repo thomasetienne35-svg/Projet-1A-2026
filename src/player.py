98a0f2990985a8939f33e58d97eea1f92233fbf4
Thomas Etienne <thomasetienne35@gmail.com>
2026-04-07 16:01:47 +0200

    loader tennis lol

--- src/player.py
+++ src/player.py
@@ -4,8 +4,18 @@ import pickle
 
 import numpy as np
 import pandas as pd
+
 pd.options.display.max_columns = 100
 
+
 class Player:
-    def __init__(self, age, taille, date_naissance):
-        
+    def __init__(
+        self, prenom_nom, nationalite, date_naissance, genre, taille, poids, team_id
+    ) -> None:
+        self.prenom_nom = prenom_nom
+        self.nationalite = nationalite
+        self.date_naissance = date_naissance
+        self.genre = genre
+        self.taille = taille
+        self.poids = poids
+        self.team_id = team_id
