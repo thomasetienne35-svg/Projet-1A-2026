88fbe3c47a0e403b5cf46c7a8bbad5dc629396b9
El-Genius-roh-roh <julesfrulli@gmail.com>
2026-04-12 22:40:19 +0200

    Correction de bugs dans le code

--- src/loaders_match/football_match_loader.py
+++ src/loaders_match/football_match_loader.py
@@ -1,11 +1,7 @@
 from match import Match
-from sport import Sport
-
 import pandas as pd
 
 pd.options.display.max_columns = 100
-
-# Ce qu'on veut mettre dans notre Player : Nom, nationalité, date_naissance, genre
 
 
 class FootballMatchLoader:
@@ -15,14 +11,19 @@ class FootballMatchLoader:
     def load_all_match(self):
         res = []
         df_football = pd.read_csv(
-            "/home/onyxia/work/Projet-1A-2026/data/football_european_leagues_tdd/match.csv"
-        )
+            r"D:\Projet-1A-2026\data\football_european_leagues_tdd\match.csv"
+        )  # Il faut trouver une solution pour cette ligne car l'emplacement de notre fichier de donnée va changer en
+        # en fonction de là où on clone le repo
         for i in range(len(df_football)):
             match = Match(None, "football", None, None)
             match.id = df_football.loc[i, "id"]
-            colonnes_home = ["home_team_goal"] + [f"home_player_{i}" for i in range (1,12)]
-            match.list_home_player = df.football.loc[i, colonnes_home].tolist()
-            colonnes_away = ["away_team_goal"] + [f"away_player_{i}" for i in range (1,12)]
-            match.list_away_player = df_football.loc[i,colonnes_away].tolist()
+            colonnes_home = ["home_team_goal"] + [
+                f"home_player_{i}" for i in range(1, 12)
+            ]
+            match.list_home_player = df_football.loc[i, colonnes_home].tolist()
+            colonnes_away = ["away_team_goal"] + [
+                f"away_player_{i}" for i in range(1, 12)
+            ]
+            match.list_away_player = df_football.loc[i, colonnes_away].tolist()
             res.append(match)
         return res
