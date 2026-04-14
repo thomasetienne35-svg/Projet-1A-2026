cacdd417de7cef84c59d8dc68c0a889684f44600
El-Genius-roh-roh <julesfrulli@gmail.com>
2026-04-14 22:51:00 +0200

    Modification du football loader pour qu'il charge le nom des joueurs

--- src/loaders_match/football_match_loader.py
+++ src/loaders_match/football_match_loader.py
@@ -18,16 +18,52 @@ class FootballMatchLoader:
             r"D:\Projet-1A-2026\data\football_european_leagues_tdd\player.csv"
         )
         for i in range(len(df_football)):
-            match = Match(None, "football", None, None)
-            match.id = df_football.loc[i, "id"]
+            match = Match(
+                None, "football", [], []
+            )  # On initialise les listes de joueurs à [] et non None pour pouvoir utiliser .append()
+            match.id = int(df_football.loc[i, "id"])
+
+            # --------Joueur à domicile------------
             colonnes_home = ["home_team_goal"] + [
-                f"home_player_{i}" for i in range(1, 12)
+                f"home_player_{j}" for j in range(1, 12)
             ]
-            liste_id_joueur = df_football.loc[i, colonnes_home].tolist()
-            match.list_home_player.append(df_joueur.loc[, ])
+            liste_id_joueur_home = df_football.loc[
+                i, colonnes_home
+            ].tolist()  # On met tous les ID des joueurs dans une liste
+            for id_joueur_home in range(
+                len(liste_id_joueur_home)
+            ):  # On récupère ensuite ces ID pour aller chercher le nom des joueurs dans le dataframe des joueurs et l'ajouter au match
+                if not pd.isna(
+                    liste_id_joueur_home[id_joueur_home]
+                ):  # pd.isna() permet de filtrer les NaN (valeurs manquantes pandas), contrairement à "is not None" qui ne les détecte pas
+                    nom_joueur_home = df_joueur.loc[
+                        df_joueur["player_api_id"]
+                        == liste_id_joueur_home[id_joueur_home],
+                        "player_name",
+                    ]  # .loc renvoie une Series pandas, on vérifie donc qu'elle n'est pas vide avant d'extraire la valeur avec .values[0]
+                    if not nom_joueur_home.empty:
+                        match.list_home_player.append(nom_joueur_home.values[0])
+
+            # --------Joueur à l'extérieur--------------
             colonnes_away = ["away_team_goal"] + [
-                f"away_player_{i}" for i in range(1, 12)
+                f"away_player_{j}" for j in range(1, 12)
             ]
-            match.list_away_player = df_football.loc[i, colonnes_away].tolist()
+            liste_id_joueur_away = df_football.loc[
+                i, colonnes_away
+            ].tolist()  # On met tous les ID des joueurs dans une liste
+            for id_joueur_away in range(
+                len(liste_id_joueur_away)
+            ):  # On récupère ensuite ces ID pour aller chercher le nom des joueurs dans le dataframe des joueurs et l'ajouter au match
+                if not pd.isna(
+                    liste_id_joueur_away[id_joueur_away]
+                ):  # pd.isna() permet de filtrer les NaN (valeurs manquantes pandas), contrairement à "is not None" qui ne les détecte pas
+                    nom_joueur_away = df_joueur.loc[
+                        df_joueur["player_api_id"]
+                        == liste_id_joueur_away[id_joueur_away],
+                        "player_name",
+                    ]  # .loc renvoie une Series pandas, on vérifie donc qu'elle n'est pas vide avant d'extraire la valeur avec .values[0]
+                    if not nom_joueur_away.empty:
+                        match.list_away_player.append(nom_joueur_away.values[0])
+
             res.append(match)
         return res
