8253257ae97cb378442a1e05a5d2408e79ec4f64
El-Genius-roh-roh <julesfrulli@gmail.com>
2026-04-28 15:02:34 +0200

    changement basket loader

--- src/loaders_match/basketball_match_loader.py
+++ src/loaders_match/basketball_match_loader.py
@@ -23,11 +23,19 @@ class BasketballMatchLoader:
         for i in range(len(df_basketball)):
             match = Match(None, "basketball", None, None)
             match.id = df_basketball.loc[i, "game_id"]
-            id_home = df.basketball.loc[i, "team_id_home"]
-            id_away = df.basketball.loc[i, "team_id_away"]
-            joueur_home_lignes = df_basketball_joueurs[df_basketball_joueurs["team_id_home"] == id_home]
-            joueur_away_lignes = df_basketball_joueurs[df_basketball_joueurs["team_id_away"] == id_away]
-            match.list_home_player = (joueur_home_lignes["first_name"] + " " + joueur_home_lignes["last_name"]).tolist()
-            match.list_away_player = (joueur_away_lignes["first_name"] + " " + joueur_away_lignes["last_name"]).tolist()
+            id_home = df_basketball.loc[i, "team_id_home"]
+            id_away = df_basketball.loc[i, "team_id_away"]
+            joueur_home_lignes = df_basketball_joueurs[
+                df_basketball_joueurs["team_id_home"] == id_home
+            ]
+            joueur_away_lignes = df_basketball_joueurs[
+                df_basketball_joueurs["team_id_away"] == id_away
+            ]
+            match.list_home_player = (
+                joueur_home_lignes["first_name"] + " " + joueur_home_lignes["last_name"]
+            ).tolist()
+            match.list_away_player = (
+                joueur_away_lignes["first_name"] + " " + joueur_away_lignes["last_name"]
+            ).tolist()
             res.append(match)
         return res
