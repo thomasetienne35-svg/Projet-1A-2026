d228e750b1a3a25488d613049e4fe0826193efc2
Thomas Etienne <thomasetienne35@gmail.com>
2026-05-08 14:34:08 +0200

    màj

--- test/test_statistiques/test_details_match.py
+++ test/test_statistiques/test_details_match.py
@@ -1,8 +1,6 @@
 import os
 import sys
-import os
 
-# On force Python à chercher les modules directement dans le dossier "src"
 sys.path.insert(0, os.path.abspath("src"))
 
 from types import SimpleNamespace
